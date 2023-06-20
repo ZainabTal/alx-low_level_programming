33795a3a224bd8d26adb84f28bd683a3082b3be7
ZainabTal <talmzipc2@gmail.com>
2023-06-20 16:23:25 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "School.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar('S');
+	_putchar('c');
+	_putchar('h');
+	_putchar('o');
+	_putchar('o');
+	_putchar('l');
+	_putchar('\n');
+
+	return (0);
+}
